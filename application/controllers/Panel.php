30230a2e4f53dfc7e6489f9bb19f2b1d2410a5e2
khristoff <contacto@khristoff.com>
2018-03-25 18:39:52 -0600

    Se creó controlador para el Panel

---
+++ application/controllers/Panel.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Panel extends CI_Controller {
+	public function index(){
+		$this->load->view('welcome_message');
+	}
+}
